705ad0bba15cdc1fd3aad52b863ea4af06e3456b
malcov-girl <ronny28nov@gmail.com>
2022-10-29 12:17:34 +0530

    gradient(#349beb,#c334eb) Product view added fully functional cart

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -6,7 +6,9 @@ const Product = ({ product: { image, name, slug, price } }) => {
         <div>
             <Link href={`/product/${slug.current}`} >
                 <div className="product-card">
-                    <img src={urlFor(image && image[0])} width={250} height={250} className="product-image" alt="image" />
+                    {image && (
+                        <img src={urlFor(image && image[0])} width={250} height={250} className="product-image" alt="image" />
+                    )}
                     <p className='product-name'>{name}</p>
                     <p className='product-price'>${price}</p>
                 </div>
